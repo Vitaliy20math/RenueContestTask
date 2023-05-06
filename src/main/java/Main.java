c7bd516d2e0455c7d76a18b99d0f761a3d20f83f
Vitaliy <akhkyamiev@inbox.ru>
2023-05-07 02:51:50 +0400

    geting result but really lot by time x2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,16 +11,16 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        //System.out.println("Please enter your filter: ");
-        while(true) {
+
+        while (true) {
+            System.out.println("Please enter your filter: ");
             String filter = scanner.nextLine();
             if (filter.contains("!quit")) {
                 break;
             }
-
+            System.out.println("Please enter name airport: ");
             String nameAirport = scanner.nextLine();
-            System.out.println(nameAirport.toString());
-            HashMap<String, List<List<Object>>>hashMap = new HashMap<>();
+            HashMap<String, List<List<Object>>> hashMap = new HashMap<>();
 
             String csvFilePath = "D://airports.csv";
             List<Object> data;
@@ -30,43 +30,114 @@ public class Main {
                 while ((line = br.readLine()) != null) {
                     String newLine = line.trim();
                     String[] columns;
-                    columns =newLine.split(",");
+                    columns = newLine.split(",");
                     data = new ArrayList<>(Arrays.asList(columns));
                     if (hashMap.containsKey(columns[1])) {
                         hashMap.get(columns[1]).add(data);
                     } else {
                         List<List<Object>> list = new ArrayList<>();
                         list.add(data);
-                        hashMap.put(columns[1],list);
+                        hashMap.put(columns[1], list);
                     }
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
+            int countGoodLine = 0;
+            long elapsedTime = 0l;
+            HashMap<String, List<List<Object>>> solve = new HashMap<>();
+            long startTime = System.nanoTime();
             for (String key : hashMap.keySet()) {
                 if (key.replaceAll("\"", "").startsWith(nameAirport)) {
                     if (filter.isEmpty()) {
+                        ++countGoodLine;
                         System.out.println(hashMap.get(key).size() + " " + key + hashMap.get(key));
                     } else {
                         List<String> tokens = convertToRPN(filter);
-                        System.out.println("Our tokens: ");
-                        System.out.println(tokens);
-                        //String output = convertToRPN(filter);
-                        //System.out.println(output);
 
-                        /*List<List<Object>> list = hashMap.get(key);
-                        for (int i = 0; i < list.size(); ++i) {
-                            list.get(i).
-                        }*/
+                        Stack<Boolean> stack = new Stack<>();
+                        List<Integer> numLine = new ArrayList<>();
+                        int columnIndex;
+
+                        for (int i = 0; i < hashMap.get(key).size(); ++i) {
+                            for (String token : tokens) {
+
+                                if (!token.equals("||") && !token.equals("&")) {
+
+                                    columnIndex = Integer.parseInt(token.substring(token.indexOf("[") + 1, token.indexOf("]")).trim()) - 1;
+                                    if (token.contains("=")) {
+                                        String value = token.substring(token.indexOf("=") + 1).replaceAll("['’\"]", "").trim();
+                                        String valueInCollection = ((String) hashMap.get(key).get(i).get(columnIndex)).replaceAll("\"", "").replaceAll("\s+","");
+
+                                        if (valueInCollection.equalsIgnoreCase(value)) {
+                                            stack.push(true);
+                                            //System.out.println("here");
+                                        } else {
+                                            //System.out.println("again here");
+                                            stack.push(false);
+                                        }
+                                    }
+                                    if (token.contains("<>")) {
+                                        String value = token.substring(token.indexOf("<>") + 1).trim();
+                                        if (hashMap.get(key).get(i).get(columnIndex) != value.replaceAll("\"", "")) {
+                                            stack.push(true);
+                                        } else {
+                                            stack.push(false);
+                                        }
+                                    }
+                                    if (token.contains(">")) {
+                                        String value = token.substring(token.indexOf(">") + 1).trim();
+
+                                        if (Double.parseDouble((String) hashMap.get(key).get(i).get(columnIndex)) > Double.parseDouble(value.replaceAll("\"", ""))) {
+                                            stack.push(true);
+                                        } else {
+                                            stack.push(false);
+                                        }
+                                    }
+                                    if (token.contains("<")) {
+                                        String value = token.substring(token.indexOf("<") + 1).trim();
+                                        if (Double.parseDouble((String) hashMap.get(key).get(i).get(columnIndex)) < Double.parseDouble(value.replaceAll("\"", ""))) {
+                                            stack.push(true);
+                                        } else {
+                                            stack.push(false);
+                                        }
+                                    }
+                                } else if (token.equals("||")) {
+                                    boolean right = stack.pop();
+                                    boolean left = stack.pop();
+                                    stack.push(left || right);
+                                } else {
+                                    boolean right = stack.pop();
+                                    boolean left = stack.pop();
+                                    stack.push(left && right);
+                                }
+                            }
+                            if (stack.pop()) {
+                                ++countGoodLine;
+                                if (!solve.containsKey(key)) {
+                                    List<List<Object>> goodLinesForKey = new ArrayList<>();
+                                    List<Object> list = hashMap.get(key).get(i);
+                                    goodLinesForKey.add(list);
+                                    solve.put(key, goodLinesForKey);
+                                } else {
+                                    solve.get(key).add(hashMap.get(key).get(i));
+                                }
+                            }
+                        }
                     }
                 }
             }
-
+            for (Map.Entry<String, List<List<Object>>> s : solve.entrySet()) {
+                System.out.println(s.getKey() + s.getValue());
+            }
+            long endTime = System.nanoTime();
+            elapsedTime = endTime - startTime;
+            System.out.printf("Количество найденных строк: %d", countGoodLine);
+            System.out.println();
+            System.out.println("Время выполнения программы: " + elapsedTime / Math.pow(10, 6) + "мс");
         }
-
-
     }
+
     private static final Map<String, Integer> precedence = Map.of(
             "(", 1,
             "&", 2,
@@ -77,8 +148,7 @@ public class Main {
         Stack<String> stack = new Stack<>();
         List<String> list;
         String[] tokens = expression.split("(?<=[\\|\\|]{2})|(?=[\\|\\|]{2})|(?<=[\\&])|(?=[\\&])|(?<=[\\(])|(?=[\\(])|(?<=[\\)])|(?=[\\)])");
-        //System.out.println(Arrays.toString(Arrays.stream(tokens).toArray()));
-
+        //System.out.println(Arrays.toString(tokens));
         StringBuilder output = new StringBuilder();
 
         for (String token : tokens) {
@@ -103,8 +173,9 @@ public class Main {
                     stack.push(token);
                     break;
                 }
-                default: { // операнд
-                    output.append(token).append(" ");
+                default: {
+                    String newToken = token.replaceAll("\s+", "");
+                    output.append(newToken).append(" ");
                     break;
                 }
             }
@@ -113,8 +184,7 @@ public class Main {
             output.append(stack.pop()).append(" ");
         }
         list = Arrays.stream(output.toString().split("\\s+")).toList();
+        //System.out.println(Arrays.toString(list.toArray()));
         return list;
-        //return output.toString().trim();
     }
-
 }
